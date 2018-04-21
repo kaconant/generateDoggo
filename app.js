2a175dc9d3ea10a197b248af3f1edcd018c51d46
Krissy Conant <krissy@memberclicks.com>
2018-04-21 11:13:55 -0400

    update so it works but cards are weird

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 $(function() {
-    var renderDoggo = function() {
-        var finalHTML = "";
-        finalHTML += "<div class='card' style='width: 18rem;'>";
-        finalHTML += "<img class='card-img-top' src='" + data.message + "'</div>";
-        return finalHTML;
-    };
-
     $(".mainButton").on("click", function() {
         $(this).html("Generating Doggo...");
         $.get("https://dog.ceo/api/breeds/image/random", function(data) {
+            var renderDoggo = function() {
+                var finalHTML = "";
+                finalHTML += "<div class='card' style='width: 18rem;'>";
+                finalHTML += "<img class='card-img-top dogCard' src='" + data.message + "'</div>";
+                return finalHTML;
+            };
             var dogImage = renderDoggo(data.message);
             $(".card-deck").append(dogImage);
             $(".mainButton").html("Doggo Activate!");
@@ -22,8 +21,25 @@ adam's way:
 
 $(function() {
 
+    // page has loaded
+    $.get("https://dog.ceo/api/breeds/list", function(response) {
+        response.message.forEach(function(currentBreed) {
+            $("select").append("<option val='" + currentBreed + "'>" + currentBreed + </option>");
+            });
+        });
+
+    $("select").change(function() {
+        var dogBreed = $(this).val();
+        var url = "https://dog.ceo/api/breed/" + dogBreed + "/image/random";
+        $.get(url, function(response) {
+            $("body").append("<img src='" + response.message + ' />");
+            )
+        });
+    });
+
     var dogImage = "";
 
+    // notify 
     $(".mainButton").click(function() {
         $.get("https://dog.ceo/api/breeds/image/random", function(response) {
             dogImage = response.message;
